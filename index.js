c77d3dfd39fa678a202660c8eb6a5be1ca5a337a
Kindred Lee <kindred4lee@gmail.com>
2021-10-26 15:02:35 -0700

    list of coins

--- index.js
+++ index.js
@@ -8,19 +8,38 @@ let requestOptions = {
   //This API pulls the top 100 MarketCap cryptocurrencies 
   const base_URL = 'https://api.coincap.io/v2/assets/'
   
+  // DOM selectors
+
 
   //GET json data from API and parse into objects
 
-  fetch(base_URL, requestOptions)
+function getAllInfo() {
+  return fetch(base_URL, requestOptions)
     .then(response => response.json())
-    .then(result => console.log(result))
+    .then(result => result)
     .catch(error => console.log('error', error));
- 
+}
   
     function getOneCoin(coinName){
-      fetch(base_URL + `${coinName}`)
+      return fetch(base_URL + `${coinName}`)
       .then(response => response.json())
       .then(res => console.log(res))
     }
 
-    getOneCoin('bitcoin')
+//Render Functions
+function renderAll(arr) {
+  arr.data.forEach(renderCoinList)
+}
+
+function renderCoinList(coinObj) {
+  const coin = document.createElement('li')
+  const price = parseFloat(coinObj.priceUsd).toFixed(2)
+  console.log('coin: ', coin);
+  coin.id = `${coinObj.id}`
+  coin.innerText = `${coinObj.name} | Price: ${price}`
+  coin.addEventListener('click', addToDash)
+  cryptoItemsList.appendChild(coin);
+}
+
+// Initializer
+getAllInfo().then(renderAll)
