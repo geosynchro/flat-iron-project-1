fe2123d62d7e4ec6107ea28e5469bfdaf14cbb96
Nicholas Fagner <nicholasfagner@gmail.com>
2021-10-28 10:26:21 -0600

    Ticker working, Table data integrated

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ const base_URL = 'https://api.coincap.io/v2/assets/'
  
 const coinList = document.querySelector('#coinlist')
 const featCoin = document.querySelector('#featured-coin')
-
-//featured details selectors
 const featured= document.querySelector("#featured-coin")
 const detName = document.querySelector('#detName')
 const detSym = document.querySelector('#coinSym')
@@ -16,7 +14,8 @@ const perChange = document.querySelector('#perChange')
 const markCap = document.querySelector('#markCap')
 const searchForm = document.querySelector('#coinSearch')
 const displayedCoin = document.querySelector('#displayedCoin')
-
+const coinsTable = document.querySelector('.table')
+const ticker = document.querySelector('.crypto-scroll')
 
 
 //Listeners
@@ -41,36 +40,52 @@ function getOneCoin(coinName){
 
 //Renderers
 
-function renderAllCoins(coinArr){
-  // console.log(coinArr)
-  coinArr.data.forEach(renderOneCoin)
+function renderAllCoins(coinsArr) {
+  coinsArr.data.forEach(renderCoinList)
+  coinsArr.data.slice(0, 20).forEach(addCoinToTicker)
 }
 
-function renderOneCoin(coinObj){
-  const newCoin = document.createElement('li')
-  const favBtn = document.createElement('button')
-  favBtn.id = 'favBtn'
-  favBtn.textContent = 'Favorite'
-  const coinName = coinObj.name
-  const price = parseFloat(coinObj.priceUsd).toFixed(2)
-  const change = parseFloat(coinObj.changePercent24Hr).toFixed(2)
-  newCoin.textContent = `${coinName}  |  Price(USD):${price}  |  
-                          Change(last 24hrs):${change}% 
-                        `                
-  favBtn.addEventListener('click', handleAddDash)                      
+function renderCoinList(coinObj) {
+  const coin = document.createElement('tr')
+  const coinName = document.createElement('td')
+  const coinSym = document.createElement('td')
+  const coinPrice = document.createElement('td')
+  const coinMarketCap = document.createElement('td')
+  const coinPercent = document.createElement('td')
+  const coinFav = document.createElement('td')
+  const addFav = document.createElement('button')
   
-  newCoin.addEventListener('click', () => displayDetails(coinObj))
-
-  newCoin.appendChild(favBtn)
-  coinList.append(newCoin)
+  const price = parseFloat(coinObj.priceUsd).toFixed(2)
+  const marketPrice = parseFloat(coinObj.marketCapUsd).toFixed(2)
+  const changPerc = parseFloat(coinObj.changePercent24Hr).toFixed(2)
+  
+  coinName.innerText = `${coinObj.name}`
+  coinSym.innerText = `${coinObj.symbol}`
+  coinPrice.innerText =`$${price}`
+  coinMarketCap.innerText =`$${marketPrice}`
+  coinPercent.innerText =`${changPerc}%`
+  addFav.innerText = 'Add to Dash'
+  
+  
+  coin.addEventListener('click', () => displayDetails(coinObj))
+  addFav.addEventListener('click', handleAddDash)
+  
+  coinFav.appendChild(addFav)
+  coin.appendChild(coinName)
+  coin.appendChild(coinSym)
+  coin.appendChild(coinPrice)
+  coin.appendChild(coinMarketCap)
+  coin.appendChild(coinPercent)
+  coin.appendChild(coinFav)
+  coinsTable.appendChild(coin)
 }
 
 function displayDetails(coinObj){
   detName.textContent = coinObj.name
   detSym.textContent = `Ticker symbol: ${coinObj.symbol}`
-  curPrice.textContent = `Current Price: ${(parseFloat(coinObj.priceUsd)).toFixed(2)}`
-  perChange.textContent = `Percent Change(24hrs): ${parseFloat(coinObj.changePercent24Hr).toFixed(2)}`
-  markCap.textContent = `Total Market Cap: ${parseFloat(coinObj.marketCapUsd).toFixed(2)}`
+  curPrice.textContent = `Current Price: $${(parseFloat(coinObj.priceUsd)).toFixed(2)}`
+  perChange.textContent = `Percent Change(24hrs): ${parseFloat(coinObj.changePercent24Hr).toFixed(2)}%`
+  markCap.textContent = `Total Market Cap: $${parseFloat(coinObj.marketCapUsd).toFixed(2)}`
   // console.log(coinObj)
 }
 
@@ -83,23 +98,39 @@ function displaySearch(coinObj){
   favBtn.textContent = 'Favorite'
   dltBtn.id = "dltBtn"
   dltBtn.textContent = "Remove"
+
   const coinName = coinObj.name
   const price = parseFloat(coinObj.priceUsd).toFixed(2)
   const change = parseFloat(coinObj.changePercent24Hr).toFixed(2)
-  newCoin.textContent = `${coinName}  |  Price(USD):${price}  |  
-                          Change(last 24hrs):${change}% 
-                        `                
+  newCoin.textContent = `${coinName}  |  Price(USD):${price}  |  Change(last 24hrs):${change}%`                
   favBtn.addEventListener('click', handleAddDash)                      
   dltBtn.addEventListener('click', handleRemove)
   newCoin.addEventListener('click', () => displayDetails(coinObj))
-
-
+  
   newCoin.appendChild(favBtn)
   newCoin.appendChild(dltBtn)
   displayedCoin.appendChild(newCoin)
   return newCoin;
 }
 
+function addCoinToTicker(coinObj){
+  console.log(coinObj)
+  const spanCoin = document.createElement('span')
+  const sym = coinObj.symbol
+  const price = parseFloat(coinObj.priceUsd).toFixed(2)
+  const percChange = parseFloat(coinObj.changePercent24Hr).toFixed(2)
+  spanCoin.id = "tickCoin"
+  spanCoin.textContent = ` ${sym}: $${price}, ${percChange}%(24hrs)   |`
+  if(percChange > 0){
+    spanCoin.textContent.fontcolor('green')
+  }else{
+    spanCoin.textContent.fontcolor('red')
+  }
+  spanCoin.addEventListener('click', () => displayDetails(coinObj))
+  ticker.append(spanCoin)
+
+  // console.log(coinObj)
+}
 
 
 
@@ -122,15 +153,15 @@ function handleAddDash(e){
 
 function handleSearch(e){
   e.preventDefault();
-
+  
   let query = document.querySelector('#searchHere').value
   
   searchForm.reset();
-
+  
   fetch(base_URL +`${query}`)
   .then(resp => resp.json())
   .then(coins => displaySearch(coins.data));
-    
+  
   console.log('clicked')
 }
 
@@ -143,3 +174,28 @@ function handleSearch(e){
 getAllCoins().then(renderAllCoins)
 getOneCoin('bitcoin')
 
+
+
+// function renderAllCoins(coinArr){
+//   // console.log(coinArr)
+//   coinArr.data.forEach(renderOneCoin)
+// }
+
+// function renderOneCoin(coinObj){
+  //   const newCoin = document.createElement('li')
+  //   const favBtn = document.createElement('button')
+  //   favBtn.id = 'favBtn'
+  //   favBtn.textContent = 'Favorite'
+  //   const coinName = coinObj.name
+  //   const price = parseFloat(coinObj.priceUsd).toFixed(2)
+  //   const change = parseFloat(coinObj.changePercent24Hr).toFixed(2)
+  //   newCoin.textContent = `${coinName}  |  Price(USD):${price}  |  
+  //                           Change(last 24hrs):${change}% 
+//                         `                
+//   favBtn.addEventListener('click', handleAddDash)                      
+  
+//   newCoin.addEventListener('click', () => displayDetails(coinObj))
+
+//   newCoin.appendChild(favBtn)
+//   coinList.append(newCoin)
+// }
